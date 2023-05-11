a408645291c12008a17b39f2189f9e8e9824ab2a
Wan <wwzj@zju.edu.cn>
2023-05-11 22:37:17 +0800

    Fix GeneratingMaze

--- src/GeneratingMaze.c
+++ src/GeneratingMaze.c
@@ -4,56 +4,20 @@
 #ifndef GENERATINGMAZE_C
 #define GENERATINGMAZE_C
 
+#include<stdlib.h>
+#include<time.h>
 #include<math.h>
 #include<windows.h>
 #include "Game.h"
 
 extern int N;
 extern int mp[maxn][maxn];
-//extern const int d[4][2];
-
 //控制复杂度，rank数值越大复杂度越低，>=0
 static int Rank=0;
 //存储地图 0表示墙 1表示路 3表示陷阱 -1表示起点，-2表示终点
 typedef enum{
-    Route,Wall,Trap     //!!! 
+    Route,Wall,Trap     
 }signs;
-
-void GeneratingMaze() 
-{
-   srand(GetTickCount());
-   for (int i=1; i<N; ++i)
-    {
-        mp[i][1] = Route;
-        mp[1][i] = Route;
-        mp[N][i] = Route;
-        mp[i][N] = Route;
-    }
-    MazeDfs(1,1); //  起点定为(1,1)
-    while(1)      //  随机出口
-    {
-        int i = rand()%(N/2), j = rand()%(N/2);
-        if( i>1 && j>1 && mp[N-i][N-j] == Route )
-        {
-            mp[N-i][N-j] = -2;
-            break;
-        }
-    }
-    for ( int k=pow(N/6,2); k>0; k--) //临时想法，添加随机多个陷阱
-    {
-        while (1)
-        {
-           int i = rand()%(N-2), j = rand()%(N-2);
-            if (i>1&&j>1&&mp[N-i][N-j]==Route) 
-		    {
-			    mp[N-i][N-j]=3;
-			    break;
-		    }
-
-        }  
-    }
-
-}
 
 void MazeDfs(int x, int y) // 从（x,y) 开始生成地图
 {
@@ -89,12 +53,51 @@ void MazeDfs(int x, int y) // 从（x,y) 开始生成地图
             if ( count >= 2 ) break;
         
             range--;
-            mp[dx][dx] = Route;
+            mp[dx][dy] = Route;
         }
         if ( range <= 0 ) MazeDfs( dx, dy ); //继续以此节点递归
 
     }
     
+}
+
+void GeneratingMaze() 
+{
+   srand(GetTickCount());
+   for(int i=1; i<=N; ++i )
+        for(int j=1; j<=N; ++j)
+            mp[i][j] = 1;
+   for (int i=1; i<N; ++i)
+    {
+        mp[i][1] = Route;
+        mp[1][i] = Route;
+        mp[N][i] = Route;
+        mp[i][N] = Route;
+    }
+    MazeDfs(3,3); //  起点定为(1,1)会产生环，出错
+    while(1)      //  随机出口
+    {
+        int i = rand()%(N/2), j = rand()%(N/2);
+        if( i>1 && j>1 && mp[N-i][N-j] == Route )
+        {
+            mp[N-i][N-j] = -2;
+            break;
+        }
+    }
+    for ( int k=pow(N/6,2); k>0; k--) //临时想法，添加随机多个陷阱
+    {
+        while (1)
+        {
+           int i = rand()%(N-2), j = rand()%(N-2);
+            if (i>1&&j>1&&mp[N-i][N-j]==Route) 
+		    {
+			    mp[N-i][N-j]=3;
+			    break;
+		    }
+
+        }  
+    }
+
 }
 
 #endif
